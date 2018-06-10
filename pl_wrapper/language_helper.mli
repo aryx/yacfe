f493c226e71f227f65d723d8f94acedb1cb318ea
pad <yoann.padioleau@gmail.com>
2018-06-10 13:34:01 -0700

    * pl_wrapper: delete, no more needed if support only one language

--- pl_wrapper/language_helper.mli
+++
@@ -1,8 +0,0 @@
-val conv_c_parsing_stat: 
-  Parsing_stat.parsing_stat -> Statistics_parsing.parsing_stat
-
-val conv_cplusplus_parsing_stat: 
-  Parsing_stat2.parsing_stat -> Statistics_parsing.parsing_stat
-
-val conv_java_parsing_stat: 
-  Parse_java.parsing_stat -> Statistics_parsing.parsing_stat
